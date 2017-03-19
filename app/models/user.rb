e0cdd7cd35b5f483e64579462d6a670fdd0465a9
HEROGWP <pp820819@gmail.com>
2017-03-19 22:38:34 +0800

    sign up can sent confirmable email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   store :fb_raw_data, :accessors => [:provider, :uid, :info, :credentials, :extra]
