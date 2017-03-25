9a4dc8f9525ea6e7053ac41c3f86216b622ba8ff
HEROGWP <pp820819@gmail.com>
2017-03-25 17:31:19 +0800

    可以新增店家

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,4 +4,30 @@ class ShopsController < ApplicationController
     @shops = Shop.all
   end
 
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(permit_shop)
+
+    if @shop.save
+      flash[:notice] = "新增成功"
+      redirect_to shops_path
+    else
+      flash[:alert] = "新增失敗"
+      render :new
+    end
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def permit_shop
+    params.require(:shop).permit(:name, :phone, :address, :rate, :lng, :lat)
+  end
+
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,10 @@
 class Shop < ApplicationRecord
+  validates_presence_of :name, :lat, :lng, :message => "不能是空！！！"
+  before_save :set_location
+
   belongs_to :user
+
+  def set_location
+    self.location = "POINT(#{self.lng} #{self.lat})"
+  end
 end
