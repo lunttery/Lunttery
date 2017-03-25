6f70d399705b5d6e4aed084301829f535309267e
HEROGWP <pp820819@gmail.com>
2017-03-25 14:48:23 +0800

    設定shop的model

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module Lunttery
     # -- all .rb files in that directory are automatically loaded.
 
     config.time_zone = "Taipei"
+
+    RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
+      # By default, use the GEOS implementation for spatial columns.
+      config.default = RGeo::Geos.factory_generator
+
+      # But use a geographic implementation for point columns.
+      config.register(RGeo::Geographic.spherical_factory(srid: 4326), geo_type: "point")
+    end
   end
 end
