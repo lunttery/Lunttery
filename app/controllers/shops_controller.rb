d7824492b430b19ccf23f2300a381b8f156eda11
HEROGWP <pp820819@gmail.com>
2017-03-25 18:13:08 +0800

    可查看特定商家資訊

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,8 +1,12 @@
 class ShopsController < ApplicationController
-  before_action :set_shop, only: [:edit, :update, :destroy]
+  before_action :set_shop, only: [:show, :edit, :update, :destroy]
 
   def index
     @shops = Shop.all
+  end
+
+  def show
+
   end
 
   def new
