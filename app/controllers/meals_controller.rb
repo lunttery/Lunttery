76c23f6aa0c8b106c558ecbd7ce0f7333776392c
HEROGWP <pp820819@gmail.com>
2017-04-02 21:51:39 +0800

    可以新增修改餐點

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,48 @@
+class MealsController < ApplicationController
+  before_action :set_shop
+  before_action :set_meal, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @meal = @shop.meals.build
+  end
+
+  def create
+    @meal = @shop.meals.build(permit_meal)
+
+    if @meal.save
+      flash[:notice] = "新增成功"
+      redirect_to shop_path(@shop)
+    else
+      flash[:alert] = "新增失敗"
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @meal.update(permit_meal)
+      flash[:notice] = "編輯成功"
+      redirect_to shop_path(@shop)
+    else
+      flash[:alert] = "編輯失敗"
+      render :edit
+    end
+  end
+
+  private
+
+  def set_meal
+    @meal = @shop.meals.find(params[:id])
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def permit_meal
+    params.require(:meal).permit(:name, :price, { photos: [] })
+  end
+end
