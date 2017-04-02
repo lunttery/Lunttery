bf1bad0f7714bed332ef0e695ddf4f3952497e35
HEROGWP <pp820819@gmail.com>
2017-04-02 15:38:19 +0800

    測Shop CRUD功能正常

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,5 +1,26 @@
+# create_table "shops", force: :cascade do |t|
+#   t.string    "name"
+#   t.string    "address"
+#   t.string    "phone"
+#   t.float     "rate"
+#   t.geography "location",   limit: {:srid=>4326, :type=>"point", :geographic=>true}
+#   t.integer   "user_id"
+#   t.datetime  "created_at",                                                          null: false
+#   t.datetime  "updated_at",                                                          null: false
+#   t.float     "lng"
+#   t.float     "lat"
+#   t.index ["user_id"], name: "index_shops_on_user_id", using: :btree
+# end
+
 FactoryGirl.define do
   factory :shop do
-
+    sequence :name do |n|
+      "lunttery#{n}"
+    end
+    address '地址'
+    phone '0912345678'
+    rate 4.6
+    lng 121.3
+    lat 25.3
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence :email do |n|
+      "user#{n}@gmail.com"
+    end
+    password "12345678"
+    confirmed_at Time.now
   end
 end
